39a67942950c62faaf3627126856e052bb48cf23
edu.wmv <montzdudu@gmail.com>
2021-09-25 11:59:08 -0300

    first_page

--- pages/piramides/piramide.js
+++ pages/piramides/piramide.js
@@ -29,3 +29,15 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+/*---------------------------------------------*/
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  if(this.scrollY >= 200){
+    scrollUp.classList.add('show-scroll'); 
+  } else { 
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+window.addEventListener('scroll', scrollUp)
