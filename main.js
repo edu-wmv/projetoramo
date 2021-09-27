66d2c300fece6731ec9b53f27a6832d113624e21
edu.wmv <montzdudu@gmail.com>
2021-09-27 17:03:20 -0300

    mobile#first

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ var swiper = new Swiper(".swiper-container", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  spaceBetween: 40,
-  loop: true,
+  spaceBetween: 20,
   autoplay: {
     delay: 2500,
     disableOnInteraction: true,
   },
+  loop: true,
   coverflowEffect: {
     rotate: 0,
   }
